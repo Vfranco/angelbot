e60b9ad6f6adea579d15cf0e2278c73c2696be49
HilcaDev <hilky77@hotmail.com>
2022-03-09 01:45:14 -0300

    angelbot-logout

--- src/app/shared/pages/topbar/topbar.component.ts
+++ src/app/shared/pages/topbar/topbar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Navigation } from '@core/constants/navigataion.enum';
+import { ILocalStorageRepository } from '@domain/repository/localstorage.repository';
+import { UserDto } from '@domain/users/user.dto';
 
 @Component({
   selector: 'topbar-component',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  userSession: UserDto;
+
+  constructor(@Inject('localstorageRepository') private localstorageService: ILocalStorageRepository, private router: Router) { }
 
   ngOnInit(): void {
 
+  }
+
+  recoverySessionFromLocalStorage(): void {
+    this.userSession = this.localstorageService.getItem(Navigation.userSession);
+  }
+
+  logoutSession(): void {
+    this.localstorageService.removeItem();
+    this.router.navigate([Navigation.login]);
   }
 }
